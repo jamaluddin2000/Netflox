3c11b484136d88fb9c71935e99c9692a72f03739
Dilfa Amalia GF <dilfaamalia513@gmail.com>
2022-09-25 22:43:53 +0700

    Update Deskripsi Film
    Menambah tahun, rating, story line, dll

--- src/pages/component/Description_film.js
+++ src/pages/component/Description_film.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {View, Text, StyleSheet, TouchableOpacity, Image, TextInput} from 'react-native'
+import { ScrollView, View, Text, StyleSheet, TouchableOpacity, Image, TextInput, Dimensions } from 'react-native'
+export const {height, width} = Dimensions.get("window");
 
 function Description_film(props) {
 
@@ -8,47 +9,93 @@ function Description_film(props) {
     }
 
     return (
-        <View style={{backgroundColor: "black"}}>
-            <Text style={{color: "red"}}>
+        <ScrollView style={{ backgroundColor: "black" }}>
+            <Text style={{ color: "red" }}>
                 Logo Netflox dan Icon Pencarian
             </Text>
             <TouchableOpacity onPress={handleInput}>
-                <Text style={{color: "black", fontSize: 30}}>
+                <Text style={{ color: "black", fontSize: 30 }}>
                     Klick
                 </Text>
             </TouchableOpacity>
-        
-            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+
+            <View>
                 <Image
                     source={require('../img/gambar_film.jpeg')}
-                    style={{width: 415, height: 250}}
-                    resizeMode="contain"
+                    style={{ width: width, height: height/3 }}
+                    
                 />
             </View>
 
-            <Text style={{color: "white", fontSize: 30, textAlign: 'left', marginLeft: 10}}>
+            <Text style={styles.title}>
                 The Lion King
             </Text>
 
-            <Text style={{color: "darkgray", fontSize: 13, textAlign: 'left', marginLeft: 10, marginVertical: 5}}>
-                2022       18+           8.5 - View
-            </Text>
-           
+            <View style={{flexDirection: 'row'}}>
+                <Text style={{
+                    color: "darkgray", 
+                    fontSize: 13, 
+                    marginLeft: 10, 
+                    marginRight: 20}}>
+                    2022
+                </Text>
+
+                <Text style={{
+                    color: "darkgray", 
+                    backgroundColor: "white", 
+                    marginRight: 40}}>
+                    18+
+                </Text>
+
+                <Text style={{
+                    color: "darkgray"}}>
+                    8.5 - View
+                </Text>
+
+            </View>
+
             <Text style={{
-                color: "white", fontSize: 13, textAlign: 'center', 
-                borderWidth: 1, borderColor: "gray", borderRadius: 3, 
-                margin: 10, marginEnd: 280}}>
+                color: "white", fontSize: 13, textAlign: 'center',
+                borderWidth: 1, borderColor: "gray", borderRadius: 3,
+                marginTop: 10, marginEnd: 280
+            }}>
                 Genre
-           </Text>
+            </Text>
 
-           <Text style={{
-                color: "white", fontSize: 13, marginVertical: 5, 
-                textAlign: 'center', paddingHorizontal: 35, paddingVertical: 10, lineHeight: 18,  
-                borderBottomWidth: 1, borderTopWidth: 1, borderColor: "gray"}}>    
+            <Text style={{
+                color: "white", 
+                fontSize: 20, 
+                textAlign: 'left', 
+                marginTop: 20}}>
+                Description Film
+            </Text>
+
+            <Text style={{
+                color: "white", fontSize: 13, marginVertical: 5,
+                textAlign: 'center', paddingHorizontal: 35, paddingVertical: 10, lineHeight: 18,
+                borderBottomWidth: 1, borderTopWidth: 1, borderColor: "gray"
+            }}>
                 Doctor Strange teams up with a mysterious teenage girl from his dreams who can travel across multiverses, to battle multiple threats, including other-universe versions of himself, which threaten to wipe out millions across the multiverse. They seek help from Wanda the Scarlet Witch, Wong and others.
-           </Text>
+            </Text>
 
-        </View>
+
+            <Text style={{
+                color: "white", 
+                fontSize: 20, 
+                textAlign: 'left', 
+                marginTop: 20}}>
+                Story Line
+            </Text>
+
+            <Text style={{
+                color: "white", fontSize: 13, marginVertical: 5,
+                textAlign: 'center', paddingHorizontal: 35, paddingVertical: 10, lineHeight: 18,
+                borderBottomWidth: 1, borderTopWidth: 1, borderColor: "gray"
+            }}>
+                Doctor Strange teams up with a mysterious teenage girl from his dreams who can travel across multiverses, to battle multiple threats, including other-universe versions of himself, which threaten to wipe out millions across the multiverse. They seek help from Wanda the Scarlet Witch, Wong and others.
+            </Text>
+
+        </ScrollView>
     )
 }
 
@@ -56,6 +103,15 @@ const styles = StyleSheet.create({
     text: {
         color: "red",
         fontSize: 50
+    },
+    title: {
+        color: "white", 
+        fontSize: 30, 
+        textAlign: 'left', 
+        margin: 5 
+    },
+    title2: {
+
     }
 })
 
